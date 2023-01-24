94bec043d0241156ce0316e10d2c21c84fce9516
Boulter97 <118101244+Boulter97@users.noreply.github.com>
2023-01-24 20:22:51 +1100

    added api key

--- Assets/main.js
+++ Assets/main.js
@@ -1,3 +1,5 @@
+var apiKey = "f50913ffe5c0b70bd3897bb81bfef586";
+
 fetch(`https;//api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}'&appid=${apiKey}`)
 
 .then(response => response.json())
