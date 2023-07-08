d4d582f8588f50dcfdcc3fee4077cc887c279c78
Boulter97 <118101244+Boulter97@users.noreply.github.com>
2023-07-08 11:41:58 +1000

    added displayCurentWeather function and corrected a spelling error in the index.html file

--- Assets/main.js
+++ Assets/main.js
@@ -8,6 +8,14 @@ function fetchWeatherData(city) {
 .catch(error => console.log(error));
 }
 
+function displayCurrentWeather(data) {
+    document.getElementById('city-name').textContent = data.name;
+    document.getElementById('current-date').textContent = new Date(data.dt * 1000).toLocaleDateString();
+    document.getElementById('current-temperature').textContent = data.main.temp;
+    document.getElementById('current-humidity').textContent = data.main.humidity;
+    document.getElementById('current-wind-speed').textContent = data.wind.speed;
+}
+
 document.getElementById('search-form').addEventListener('submit', function(e) {
     e.preventDefault();
     var city = document.getElementById('city-input').value;
