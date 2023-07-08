58cc8a7e4a573584fc09cd2f119f40569534b374
Boulter97 <118101244+Boulter97@users.noreply.github.com>
2023-07-08 11:27:58 +1000

    working on fetchWeatherData search form in main.js

--- Assets/main.js
+++ Assets/main.js
@@ -1,9 +1,15 @@
 var apiKey = "f50913ffe5c0b70bd3897bb81bfef586";
-
+function fetchWeatherData(city) {
 fetch(`https;//api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}'&appid=${apiKey}`)
-
 .then(response => response.json())
 .then(data => {
 
 })
 .catch(error => console.log(error));
+}
+
+document.getElementById('search-form').addEventListener('submit', function(e) {
+    e.preventDefault();
+    var city = document.getElementById('city-input').value;
+    fetchWeatherData(city);
+});
